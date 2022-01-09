968e160f883a409262ac51376fbca62e64f69a78
Azu0925 <yamaneko564783@gmail.com>
2022-01-09 23:40:28 +0900

    不要コード削除

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ import binascii
 CRYPTO = '636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970'
 hex_list = [CRYPTO[i: i + 2] for i in range(0, len(CRYPTO), 2)]
 
-count = 0
 for i in hex_list:
   print(binascii.unhexlify(i).decode('utf-8'), end='')

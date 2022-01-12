d40ff20524c282f962731242b638dc9b2b1b1a72
Azu0925 <yamaneko564783@gmail.com>
2022-01-12 15:15:58 +0900

    ref

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import binascii
 
 CRYPTO = '636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970636c6970'
-hex_list = [CRYPTO[i: i + 2] for i in range(0, len(CRYPTO), 2)]
 
-for i in hex_list:
-  print(binascii.unhexlify(i).decode('utf-8'), end='')
+print(binascii.unhexlify(CRYPTO).decode('utf-8'))
